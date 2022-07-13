57de6c0dbdbcbf6eae93d0a9d5c906d80ab7b8fa
Selim Muhammetgulyyev <selim.mht@gmail.com>
2022-07-13 17:06:08 +0300

    Server/mock (#14)
    * Created basic dropdown
    
    * Version2
    
    * mock server setup
    
    * install new package
    
    Co-authored-by: Hornshade <bejinari.sabin@gmail.com>
    Co-authored-by: Bejinari Sabin-Mihai <61279245+Hornshade@users.noreply.github.com>

--- src/api/API.js
+++ src/api/API.js
@@ -1,1 +1,12 @@
-// aici vine api functii
+import axios from "axios";
+axios.defaults.baseURL = process.env.REACT_APP_BASE_URL;
+
+// get listing
+export const getListings = async () => {
+  try {
+    const response = await axios.get("/listings");
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
