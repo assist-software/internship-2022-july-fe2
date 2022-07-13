82c83ccc4427d331c966a292bd1a5bbf8ed4585b
Selim Muhammetgulyyev <selim.mht@gmail.com>
2022-07-13 10:37:23 +0300

    Components/button (#3)
    * updated
    
    * Button component done

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,16 +1,38 @@
-import React from 'react'
+import React from "react";
 import PropTypes from "prop-types";
-import styles from "./Button.module.scss"
+import styles from "./Button.module.scss";
+import { ReactComponent as Heart } from "../../assets/icons/heart.svg";
 
-const Button = ({primary, secondary, disabled, label}) => {
+const Button = ({ variant, label, icon, position, disabled, onClick }) => {
   return (
-    <div className={primary ? styles.button : styles.secondary}>{label}</div>
-  )
-}
+    <button
+      onClick={onClick}
+      disabled={disabled}
+      className={`${styles.button} ${styles[variant]} ${styles[position]}`}
+    >
+      {icon}
+      {label}
+    </button>
+  );
+};
 
 Button.propTypes = {
-  primary: PropTypes.string.isRequired,
-  label: PropTypes.string.isRequired
-}
+  variant: PropTypes.oneOf(["primary", "secondary", "tertiary", "destructive"])
+    .isRequired,
+  label: PropTypes.string.isRequired,
+  icon: PropTypes.node,
+  position: PropTypes.oneOf(["left", "right", "none"]),
+  disabled: PropTypes.bool,
+  onClick: PropTypes.func,
+};
 
-export default Button
+Button.defaultProps = {
+  variant: "primary",
+  label: "Button",
+  icon: <Heart />,
+  position: "none",
+  disabled: false,
+  onClick: () => {},
+};
+
+export default Button;
--- src/pages/Demo/Selim.jsx
+++ src/pages/Demo/Selim.jsx
@@ -1,7 +1,38 @@
 import React from "react";
+import Button from "../../components/Button/Button";
 
 const Selim = () => {
-  return <div>Selim</div>;
+  return (
+    <div
+      style={{
+        width: "20rem",
+      }}
+    >
+      <h1>Regular</h1>
+      <Button variant="primary" label="Button" />
+      <Button variant="secondary" label="Button" />
+      <Button variant="tertiary" label="Button" />
+      <Button variant="destructive" label="Button" />
+
+      <h2>Disabled</h2>
+      <Button disabled variant="primary" label="Button" />
+      <Button disabled variant="secondary" label="Button" />
+      <Button disabled variant="tertiary" label="Button" />
+      <Button disabled variant="destructive" label="Button" />
+
+      <h1>icon</h1>
+      <Button position="right" variant="primary" label="Button" />
+      <Button position="right" variant="secondary" label="Button" />
+      <Button position="left" variant="tertiary" label="Button" />
+      <Button position="left" variant="destructive" label="Button" />
+
+      <h1>default</h1>
+      <Button />
+      <Button />
+      <Button />
+      <Button />
+    </div>
+  );
 };
 
 export default Selim;
