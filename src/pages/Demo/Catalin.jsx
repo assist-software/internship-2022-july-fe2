e87f3336b07923c9757767d26505e1ae42e75321
Catalin <intern.catalin.prisacaru@assist.ro>
2022-07-13 16:38:47 +0300

    remove cl,scss reorganization

--- src/pages/Demo/Catalin.jsx
+++ src/pages/Demo/Catalin.jsx
@@ -35,14 +35,21 @@ const Catalin = (props) => {
       description:
         "Designers tend to cut the lorem ipsum text when it pleases them to keep their designs nice and tidy. However, in reality, content is not the same length. Small areas may not be able to house the necessary content needed. This, in turn, causes you (the designer) to spend more time reworking the layout to fit the text. Dummy text misleads future content.",
     },
+    {
+      id: 5,
+      title: "Dreamy Treehouse Above Park City asdasd asd asd",
+      location: "Suceava",
+      price: 123,
+      description:
+        "Designers tend to cut the lorem ipsum text when it pleases them to keep their designs nice and tidy. However, in reality, content is not the same length. Small areas may not be able to house the necessary content needed. This, in turn, causes you (the designer) to spend more time reworking the layout to fit the text. Dummy text misleads future content.",
+    },
   ];
   const [announces, setAnnounces] = useState(DATA);
   const [showGrid, setShowGrid] = useState(false);
-  console.log(showGrid);
   return (
     <div>
       Catalin
-      <button onClick={() => setShowGrid(true)}>Click here</button>
+      <button onClick={() => setShowGrid(!showGrid)}>Click here</button>
       {announces.map((announce) => (
         <Card
           title={announce.title}
