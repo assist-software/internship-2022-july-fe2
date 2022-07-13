d7aa99c0768b372d31d51ecd5f8123c0ab668ff4
Hornshade <bejinari.sabin@gmail.com>
2022-07-13 17:38:28 +0300

    in progress

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,22 +1,58 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import styles from "./Header.module.scss";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import logo from "../../assets/logo/logo-assist-tagline.png";
+import { Button } from "react-bootstrap";
+import Dropdown from "../Dropdown/Dropdown";
+import { Form } from "react-bootstrap";
+import heart from "../../assets/icons/heart.svg";
 
 const Header = () => {
   const navigate = useNavigate();
   return (
-    <div>
-      <ul className={styles.list}>
-        <li onClick={() => navigate("/")}>Home</li>
-        <li onClick={() => navigate("/my-account")}>MyAccount</li>
-        <li onClick={() => navigate("/listing")}>Listing</li>
-        <li onClick={() => navigate("/onboarding")}>Onboarding</li>
-        <li onClick={() => navigate("/favorites")}>Favorites</li>
-        <li onClick={() => navigate("/confirmation")}>Confirmation</li>
-        <li onClick={() => navigate("/add")}>AddEdit</li>
-        <li onClick={() => navigate("/edit")}>AddEdit</li>
-      </ul>
-    </div>
+    <Navbar collapseOnSelect expand="lg" className={styles.navbar}>
+      <Container>
+        <Navbar.Brand href="#home">
+          <img src={logo} className={styles.assist} />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <div style={{ width: "160px" }}>
+              <Dropdown
+                fontWeight="semibold"
+                fontSize="buton"
+                className={styles.drop}
+              />
+            </div>
+            <Form className="d-flex">
+              <Form.Control
+                type="search"
+                placeholder="Search"
+                className={styles.search}
+                aria-label="Search"
+              />
+            </Form>
+          </Nav>
+          <Nav>
+            <Button className={styles.favorites}>
+              <img src={heart} />
+              Favourites
+            </Button>
+            <NavDropdown title="My profile" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import App from "./App";
 import "./styles/index.scss";
 import { AuthProvider } from "./context/AuthProvider";
 
+// importing bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
