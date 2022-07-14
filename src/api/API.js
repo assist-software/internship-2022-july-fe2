9d3bfa815da59993ada32dacf0a7dfc02a409eec
selimmh <selim.mht@gmail.com>
2022-07-14 10:29:47 +0300

    Mock db setup

--- src/api/API.js
+++ src/api/API.js
@@ -4,7 +4,37 @@ axios.defaults.baseURL = process.env.REACT_APP_BASE_URL;
 // get listing
 export const getListings = async () => {
   try {
-    const response = await axios.get("/listings");
+    const response = await axios.get("/listing");
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// get user
+export const getUser = async (id) => {
+  try {
+    const response = await axios.get("/user/", id);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// deactivate user
+export const deactivateUser = async (id) => {
+  try {
+    const response = await axios.delete("/deactivate/", id);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// delete user
+export const deleteUser = async (id) => {
+  try {
+    const response = await axios.delete("/user/", id);
     return response.data;
   } catch (error) {
     console.error(error);
