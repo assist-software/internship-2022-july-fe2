811c5563953d08e38282354ad029d08ab32aa953
selimmh <selim.mht@gmail.com>
2022-07-15 09:33:15 +0300

    my account form

--- src/api/API.js
+++ src/api/API.js
@@ -14,7 +14,7 @@ export const getListings = async () => {
 // get user
 export const getUser = async (id) => {
   try {
-    const response = await axios.get("/user/", id);
+    const response = await axios.get("/user/" + id);
     return response.data;
   } catch (error) {
     console.error(error);
