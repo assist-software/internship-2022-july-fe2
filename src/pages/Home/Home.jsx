9452d131913ea3824e417bc553e651963998d7e5
Catalin <intern.catalin.prisacaru@assist.ro>
2022-07-19 17:29:58 +0300

    created new component nav for every role , created new component with grid/row icons, edit delete buttons depends on role

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,15 +1,34 @@
 import Carousel from "../../components/Carousel/Carousel";
+import Nav from "../../components/Nav/Nav";
 import Tabs from "../../components/Tabs/Tabs";
+import useAuth from "../../hooks/useAuth";
+import Listing from "../Listing/Listing";
+
 const Home = () => {
+  const { user } = useAuth();
   return (
     <div>
       <div>
         <Tabs />
+        <Nav />
       </div>
+      {user?.role === "admin" ? (
+        <Listing hideControls />
+      ) : (
+        <>
+          <Carousel title="Most view" />
+          <Carousel title="Big Houses" />
+          <Carousel title="Small Houses" />
+        </>
+      )}
 
-      <Carousel title="Most view" />
-      <Carousel title="Big Houses" />
-      <Carousel title="Small Houses" />
+      {/* {user?.role === "admin" ? (
+        <div>admin</div>
+      ) : user?.role === "user" ? (
+        <div>user</div>
+      ) : (
+        <div>guest</div>
+      )} */}
     </div>
   );
 };
