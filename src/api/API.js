7c3948976fd7f70327a894f68707d48ce2822f59
Hornshade <bejinari.sabin@gmail.com>
2022-07-20 17:49:59 +0300

    FULL SWAGGER INTO API

--- src/api/API.js
+++ src/api/API.js
@@ -1,7 +1,145 @@
 import axios from "axios";
 axios.defaults.baseURL = process.env.REACT_APP_BASE_URL;
 
-// login
+// ---------------------------- FAVORITES ----------------------------------
+// add to favorites
+export const addFavorite = async () => {
+  try {
+    const response = await axios.post("/favorite/addtofavorites", {
+      userId,
+      listingId,
+    });
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// get favorites
+export const getFavorite = async (userId) => {
+  try {
+    const response = await axios.get("/favorite/" + userId);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// delete favorites
+export const deleteFavorite = async (favoriteId) => {
+  try {
+    const response = await axios.delete("/favorite/" + favoriteId);
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// ---------------------------- LISTINGS ----------------------------------
+// add listing
+export const createListing = async (data) => {
+  try {
+    const response = await axios.post("/listing/create", data);
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// put listing by id
+export const putListingById = async (data) => {
+  try {
+    const response = await axios.put("/listing/" + data.id, data);
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// get listing
+export const getListings = async () => {
+  try {
+    const response = await axios.get("/listing");
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// get listing by id
+export const getListingById = async (id) => {
+  try {
+    const response = await axios.get("/listing/" + id);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// delete listing
+export const deleteListingById = async (id) => {
+  try {
+    const response = await axios.delete("/listing/" + id);
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// ---------------------------- MAIL ----------------------------------
+// post MAIL
+export const postMail = async (data) => {
+  try {
+    const response = await axios.post("/mail/send", data);
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// ---------------------------- MESSAGES ----------------------------------
+// post new message
+export const newMessage = async (data) => {
+  try {
+    const response = await axios.post("/message/new", data);
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// get message by listing id
+export const getMessage = async (listingId) => {
+  try {
+    const response = await axios.get("/message/" + listingId);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// delete all messages by listing id
+export const deleteAllMessages = async (listingId) => {
+  try {
+    const response = await axios.delete("/message/deleteall/" + listingId);
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+//delete one message by message id
+export const deleteMessage = async (messageId) => {
+  try {
+    const response = await axios.delete("/message/" + messageId);
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// ---------------------------- USER ----------------------------------
+// login  authenticate
 export const login = async (email, password) => {
   try {
     const response = await axios.post("/user/authenticate", {
@@ -23,6 +161,87 @@ export const register = async (email, password) => {
     return response;
   } catch (error) {
     console.log(error);
+  }
+};
+
+// reset password
+export const resetPassword = async (email) => {
+  try {
+    const response = await axios.post("/user/reset/password" + email); //posibil sa trebuiasca /user/reset/password/
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// update user
+export const updateUser = async (data) => {
+  try {
+    const response = await axios.put("/user/" + data.id, data);
+    return response;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// get user by id
+export const getUserById = async (id) => {
+  try {
+    const response = await axios.get("/user/" + id);
+    return response;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// delete user
+export const deleteUser = async (id) => {
+  try {
+    const response = await axios.delete("/user/", id);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// get user by email
+export const getUserByEmail = async (email) => {
+  try {
+    const response = await axios.get("/user/search/" + email);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// get users
+export const getUsers = async (data) => {
+  try {
+    const response = await axios.get("/user", data);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// ------------------------------MOCK-------------------------------------
+// add listing    Nu exista
+export const addListing = async (data) => {
+  try {
+    const response = await axios.post("/listing", data);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// deactivate user
+export const deactivateUser = async (id) => {
+  try {
+    const response = await axios.delete("/deactivate/", id);
+    return response.data;
+  } catch (error) {
+    console.error(error);
   }
 };
 
@@ -49,73 +268,3 @@ export const register = async (email, password) => {
 //   }
 // };
 // user - 0, admin - 1
-
-// get listing
-export const getListings = async () => {
-  try {
-    const response = await axios.get("/listing");
-    return response.data;
-  } catch (error) {
-    console.error(error);
-  }
-};
-
-// get listing by id
-export const getListingById = async (id) => {
-  try {
-    const response = await axios.get("/listing/" + id);
-    return response.data;
-  } catch (error) {
-    console.error(error);
-  }
-};
-
-// add listing
-export const addListing = async (data) => {
-  try {
-    const response = await axios.post("/listing", data);
-    return response.data;
-  } catch (error) {
-    console.error(error);
-  }
-};
-
-// get user by id
-export const getUserById = async (id) => {
-  try {
-    const response = await axios.get("/user/" + id);
-    return response;
-  } catch (error) {
-    console.error(error);
-  }
-};
-
-// update user
-export const updateUser = async (data) => {
-  try {
-    const response = await axios.put("/user/" + data.id, data);
-    return response;
-  } catch (error) {
-    console.error(error);
-  }
-};
-
-// deactivate user
-export const deactivateUser = async (id) => {
-  try {
-    const response = await axios.delete("/deactivate/", id);
-    return response.data;
-  } catch (error) {
-    console.error(error);
-  }
-};
-
-// delete user
-export const deleteUser = async (id) => {
-  try {
-    const response = await axios.delete("/user/", id);
-    return response.data;
-  } catch (error) {
-    console.error(error);
-  }
-};
