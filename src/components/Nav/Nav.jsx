817510242cb37203907bec96f65c99b74390fbd8
Catalin <intern.catalin.prisacaru@assist.ro>
2022-07-20 13:34:31 +0300

    showing data corect in all listing/my listing / pending approval (depends on role)

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,22 +1,49 @@
 import React from "react";
-import Button from "../Button/Button";
 import styles from "./Nav.module.scss";
 import useAuth from "../../hooks/useAuth";
 
-const Nav = () => {
+const Nav = ({ setView }) => {
   const { user } = useAuth();
   return (
     <div>
-      {user?.role === "user" ? (
+      {user?.role === 0 ? (
         <div className={styles.nav}>
-          <button className={styles.buttons}>All listings</button>
-          <button className={styles.buttons}>My listings</button>
+          <button
+            onClick={() => {
+              setView(false);
+            }}
+            className={styles.buttons}
+          >
+            All listings
+          </button>
+          <button
+            onClick={() => {
+              setView(true);
+            }}
+            className={styles.buttons}
+          >
+            My listings
+          </button>
         </div>
       ) : null}
-      {user?.role === "admin" ? (
+      {user?.role === 1 ? (
         <div className={styles.nav}>
-          <button className={styles.buttons}>All listings</button>
-          <button className={styles.buttons}>Pending approval</button>
+          <button
+            onClick={() => {
+              setView(false);
+            }}
+            className={styles.buttons}
+          >
+            All listings
+          </button>
+          <button
+            onClick={() => {
+              setView(true);
+            }}
+            className={styles.buttons}
+          >
+            Pending approval
+          </button>
         </div>
       ) : null}
     </div>
